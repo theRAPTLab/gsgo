b1a203ea4eab6a9fa9010bdb52c8d065f7dfc3cb
Ben Loh <ben@inquirium.net>
2020-12-08 09:59:13 -0800

    scripteditor: Move Script editor to separate component.

--- gs_packages/gem-srv/src/app/pages/ScriptEditor.jsx
+++ gs_packages/gem-srv/src/app/pages/ScriptEditor.jsx
@@ -20,11 +20,17 @@ import * as Prism from '../../util/prism_extended';
 import { CodeJar } from '../../util/codejar';
 import '../../util/prism_extended.css';
 
+/// PANELS ////////////////////////////////////////////////////////////////////
+import PanelSim from './components/PanelSim';
+import PanelSelectAgent from './components/PanelSelectAgent';
+import PanelScript from './components/PanelScript';
+import PanelInspector from './components/PanelInspector';
+
 /// TESTS /////////////////////////////////////////////////////////////////////
 // import 'modules/tests/test-parser'; // test parser evaluation
 
 // this is where classes.* for css are defined
-import { useStylesHOC } from './page-styles';
+import { useStylesHOC } from './page-xui-styles';
 
 /// CONSTANTS & DECLARATIONS //////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -33,133 +39,31 @@ const DBG = true;
 
 /// HARCODED SOURCE ///////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-const defaultText = `
-// definitions
-defBlueprint "Bunny"
-addProp frame Number 2
-useFeature Movement
-// defaults
-prop skin 'bunny.json'
-// runtime
-featureCall Movement jitterPos -5 5
-// condition test 1
-addTest BunnyTest {{ agent.prop('frame')._value }}
-ifTest BunnyTest {{ agent.prop('x').setTo(global.LibMath.sin(global._frame()/10)*100) }}
-// condition test 2
-ifExpr {{ global.LibMath.random() < 0.01 }} {{ agent.prop('y').setTo(100) }} {{ agent.prop('y').setTo(0) }}
-`.trim();
-
-const defineGlobalAgent = `
-defGlobalAgent World
-  addProp time Number 10
-  addProp daytime Boolean true
-  // runtime
-  // condition
-  when Interval 1000
-    prop time decrement
-    defCondition "memo:switch"
-    {{ prop time < 0 }}
-    prop time setTo 10
-    prop daytime invert
-`.trim();
-
-// try to make a fish!
-/** fish just wander around the screen
- *
- */
-const defineFish = `
-// define/default program
-# blueprint agent baseagent
-defBlueprint Fish
-addProp foodLevel Number 50
-prop foodLevel setMin 0
-prop foodLevel setMax 100
-prop skin setTo 'alive.png'
-useFeature Movement
-
-featureProp inputType setTo 'runtime'
-// runtime program (runs only for runtime mode?)
-featureCall Movement randomWalk 15 2
-
-// condition programs
-// every second decrement foodlevel
-when Interval 1000
-  prop foodLevel increment
-  defCondition "memo:dead"
-    {{ prop foodLevel < 1 }}
-    prop isActive false
-    prop skin setTo "dead.png"
-    featureProp inputType setTo 'static'
-  defCondition "memo:worldtimer"
-    globalAgentProp World daytime
-    {{ globalAgentProp World daytime === true}}
-    prop skin setTo "happy.png"
-`.trim();
-
-const model5 = `# BLUEPRINT Bee
-# DEFINE
-addProp frame Number 3
-useFeature Movement
-# UPDATE
-setProp skin 'bunny.json'
-featureCall Movement jitterPos -5 5
-# EVENT
-onEvent Tick [[
-  // happens every second, and we check everyone
-  ifExpr {{ agent.prop('name').value==='bun5' }} [[
-    dbgOut 'my tick' 'agent instance' {{ agent.prop('name').value }}
-    dbgOut 'my tock'
-  ]]
-  setProp 'x'  0
-  setProp 'y'  0
-]]
-# CONDITION
-when Bee sometest [[
-  // dbgOut SingleTest
-]]
-when Bee sometest Bee [[
-  // dbgOut PairTest
-]]
-
-// Bad Matches
-preaddProp
-addPropPost
-not a # pragma
-addProp comment // comment after
-`;
-
-const jsSnippet = 'function() { console.log("Hello World"): }';
-
-/// PRISM GEMSCRIPT DEFINITION ////////////////////////////////////////////////
-const keywords = DATACORE.GetAllKeywords();
-const keywords_regex = new RegExp(
-  '\\b(' + keywords.reduce((acc, cur) => `${acc}|${cur}`) + ')\\b'
-);
-console.log('PRISM gemscript keywords', keywords_regex);
-
-const types = ['Number', 'String', 'Boolean'];
-const types_regex = new RegExp(
-  '\\b(' + types.reduce((acc, cur) => `${acc}|${cur}`) + ')\\b'
-);
-console.log('PRISM gemscript types', types_regex);
 
 /// URSYS SYSHOOKS ////////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-UR.SystemHook(
-  'UR/LOAD_ASSETS',
-  () =>
-    new Promise((resolve, reject) => {
-      if (DBG) console.log(...PR('LOADING ASSET MANIFEST @ UR/LOAD_ASSETS...'));
-      (async () => {
-        let map = await GLOBAL.LoadAssets('static/assets.json');
-        if (DBG) console.log(...PR('ASSETS LOADED'));
-        // Compiler.jsx starts sim, but we shouldn't need to?
-        // SIM.StartSimulation();
-        // if (DBG) console.log(...PR('SIMULATION STARTED'));
-      })();
-      resolve();
-    })
-);
+// Is this necessary?  Causing async "Uncaught (in promise) undefined" error
+// UR.SystemHook(
+//   'UR/LOAD_ASSETS',
+//   () =>
+//     new Promise((resolve, reject) => {
+//       if (DBG) console.log(...PR('LOADING ASSET MANIFEST @ UR/LOAD_ASSETS...'));
+//       (async () => {
+//         let map = await GLOBAL.LoadAssets('static/assets.json');
+//         if (DBG) console.log(...PR('ASSETS LOADED'));
+//         // Compiler.jsx starts sim, but we shouldn't need to?
+//         // SIM.StartSimulation();
+//         // if (DBG) console.log(...PR('SIMULATION STARTED'));
+//       })();
+//       resolve();
+//     })
+// );
+
+/// PANEL CONFIGURATIONS //////////////////////////////////////////////////////
+const panelConfig = new Map();
+panelConfig.set('select', '50% auto 0px'); // columns
+panelConfig.set('script', '50% auto 0px'); // columns
+panelConfig.set('sim', '25% auto 0px'); // columns
 
 /// CLASS DECLARATION /////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -167,33 +71,17 @@ UR.SystemHook(
 class ScriptEditor extends React.Component {
   constructor() {
     super();
-    // codejar
-    this.jarRef = React.createRef();
-    this.jar = '';
+    this.state = {
+      panelConfiguration: 'select'
+    };
 
-    // The keys map to token definitions in the prism css file.
-    Prism.languages.gemscript = Prism.languages.extend('javascript', {
-      'keyword': keywords_regex,
-      'namespace': /^# \W*/gm, // multiline
-      'inserted': types_regex
-    });
-    // Prism.languages.javascript.keyword[1].pattern = /(^|[^.]|\.\.\.\s*)\b(?:as|async(?=\s*(?:function\b|\(|[$\w\xA0-\uFFFF]|$))|await|break|case|class|const|continue|debugger|default|delete|do|else|enum|export|extends|for|from|function|when|(?:get|set)(?=\s*[\[$\w\xA0-\uFFFF])|if|implements|import|in|instanceof|interface|let|new|null|of|package|private|protected|public|return|static|super|switch|this|throw|try|typeof|undefined|var|void|while|with|yield|defBlueprint|addProp|prop|setTo|useFeature|featureProp|featureCall|defCondition|increment|decrement)\b/;
-    //      'keyword': /(^|[^.]|\.\.\.\s*)\b(?:as|async(?=\s*(?:function\b|\(|[$\w\xA0-\uFFFF]|$))|when|(?:get|set)(?=\s*[\[$\w\xA0-\uFFFF])|defBlueprint|addProp|prop|setTo|useFeature|featureProp|featureCall|defCondition|increment|decrement)\b/,
+    this.OnPanelClick = this.OnPanelClick.bind(this);
   }
 
   componentDidMount() {
     document.title = 'GEMSTEP SCRIPT EDITOR';
     // start URSYS
     UR.SystemConfig({ autoRun: true });
-    // initialize codejar
-    const highlight = editor => {
-      Prism.highlightElement(editor);
-    };
-    const editor = this.jarRef.current;
-    this.jar = CodeJar(editor, highlight);
-    this.jar.onUpdate(code => {
-      this.text = code;
-    });
   }
 
   componentDidCatch(e) {
@@ -204,11 +92,11 @@ class ScriptEditor extends React.Component {
     console.log('componentWillUnmount');
   }
 
-  // echo typing in SourceText to state
-  updateText(evt) {
-    const text = evt.target.value;
-    this.text = text;
-    this.setState({ text });
+  OnPanelClick(id) {
+    console.log('click', id); // e, e.target, e.target.value);
+    this.setState({
+      panelConfiguration: id
+    });
   }
 
   /*  Renders 2-col, 3-row grid with TOP and BOTTOM spanning both columns.
@@ -216,11 +104,14 @@ class ScriptEditor extends React.Component {
    *  make this happen.
    */
   render() {
+    const { panelConfiguration } = this.state;
     const { classes } = this.props;
     return (
       <div
         className={classes.root}
-        style={{ gridTemplateColumns: '50% auto 0px' }}
+        style={{
+          gridTemplateColumns: panelConfig.get(panelConfiguration)
+        }}
       >
         <div
           id="console-top"
@@ -229,25 +120,34 @@ class ScriptEditor extends React.Component {
         >
           <span style={{ fontSize: '32px' }}>SCRIPT EDITOR</span> UGLY DEVELOPER
           MODE
+          <span
+            style={{ textAlign: 'right', display: 'inline-block', width: '100%' }}
+          >
+            <a href="/app">HOME</a>
+          </span>
         </div>
-        <div id="console-left" className={clsx(classes.cell, classes.left)}>
-          <div>
-            <pre
-              className="language-gemscript line-numbers match-braces"
-              style={{ fontSize: '10px', lineHeight: 1, whiteSpace: 'pre-line' }}
-            >
-              <code
-                id="codejar"
-                ref={this.jarRef}
-                style={{ width: '100%', height: '50vh' }}
-              >
-                {model5}
-              </code>
-            </pre>
+        <div id="console-left" className={classes.left}>
+          {panelConfiguration === 'select' && (
+            <PanelSelectAgent id="select" onClick={this.OnPanelClick} />
+          )}
+          {panelConfiguration === 'script' && (
+            <PanelScript id="script" onClick={this.OnPanelClick} />
+          )}
+          <div className={classes.instructions}>
+            <p>Script text changes are automatically compiled and run?</p>
+            <p>
+              Instance is automatically created and controllable via faketrack in
+              the Sim view. Also auto-selected as the first inspector.
+            </p>
           </div>
         </div>
         <div id="root-renderer" className={classes.main}>
-          world view
+          <PanelSim id="sim" onClick={this.OnPanelClick} />
+          <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr' }}>
+            <PanelInspector isActive />
+            <PanelInspector />
+            <PanelInspector />
+          </div>
         </div>
         <div
           id="console-bottom"
---
+++ gs_packages/gem-srv/src/app/pages/components/PanelScript.jsx
@@ -0,0 +1,205 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import UR from '@gemstep/ursys/client';
+
+/// APP MAIN ENTRY POINT //////////////////////////////////////////////////////
+import * as SIM from 'modules/sim/api-sim'; // needed to register keywords for Prism
+import * as GLOBAL from 'modules/runtime-globals';
+import * as DATACORE from 'modules/runtime-datacore';
+
+/// CODE EDIT + HIGHLIGHTING //////////////////////////////////////////////////
+import * as Prism from '../../../util/prism_extended';
+import { CodeJar } from '../../../util/codejar';
+import '../../../util/prism_extended.css';
+
+import { useStylesHOC } from '../page-xui-styles';
+
+import PanelChrome from './PanelChrome';
+
+/// CONSTANTS & DECLARATIONS //////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+const PR = UR.PrefixUtil('PANELSCRIPT');
+const DBG = true;
+
+/// DEMO SCRIPT ///////////////////////////////////////////////////////////////
+const demoscript = `# BLUEPRINT Bee
+# DEFINE
+addProp frame Number 3
+useFeature Movement
+# UPDATE
+setProp skin 'bunny.json'
+featureCall Movement jitterPos -5 5
+# EVENT
+onEvent Tick [[
+  // happens every second, and we check everyone
+  ifExpr {{ agent.prop('name').value==='bun5' }} [[
+    dbgOut 'my tick' 'agent instance' {{ agent.prop('name').value }}
+    dbgOut 'my tock'
+  ]]
+  setProp 'x'  0
+  setProp 'y'  0
+]]
+# CONDITION
+when Bee sometest [[
+  // dbgOut SingleTest
+]]
+when Bee sometest Bee [[
+  // dbgOut PairTest
+]]
+
+// Bad Matches
+preaddProp
+addPropPost
+not a # pragma
+addProp comment // comment after
+
+// EXTRA LONG SCRIPT
+# DEFINE
+addProp frame Number 3
+useFeature Movement
+# UPDATE
+setProp skin 'bunny.json'
+featureCall Movement jitterPos -5 5
+# EVENT
+onEvent Tick [[
+  // happens every second, and we check everyone
+  ifExpr {{ agent.prop('name').value==='bun5' }} [[
+    dbgOut 'my tick' 'agent instance' {{ agent.prop('name').value }}
+    dbgOut 'my tock'
+  ]]
+  setProp 'x'  0
+  setProp 'y'  0
+]]
+# CONDITION
+when Bee sometest [[
+  // dbgOut SingleTest
+]]
+when Bee sometest Bee [[
+  // dbgOut PairTest
+]]
+
+`;
+
+/// PRISM GEMSCRIPT DEFINITION ////////////////////////////////////////////////
+const keywords = DATACORE.GetAllKeywords();
+const keywords_regex = new RegExp(
+  '\\b(' + keywords.reduce((acc, cur) => `${acc}|${cur}`) + ')\\b'
+);
+console.log('PRISM gemscript keywords', keywords_regex);
+
+const types = ['Number', 'String', 'Boolean'];
+const types_regex = new RegExp(
+  '\\b(' + types.reduce((acc, cur) => `${acc}|${cur}`) + ')\\b'
+);
+console.log('PRISM gemscript types', types_regex);
+
+/// URSYS SYSHOOKS ////////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Is this necessary?  Causing async "Uncaught (in promise) undefined" error
+// UR.SystemHook(
+//   'UR/LOAD_ASSETS',
+//   () =>
+//     new Promise((resolve, reject) => {
+//       if (DBG) console.log(...PR('LOADING ASSET MANIFEST @ UR/LOAD_ASSETS...'));
+//       (async () => {
+//         let map = await GLOBAL.LoadAssets('static/assets.json');
+//         if (DBG) console.log(...PR('ASSETS LOADED'));
+//         // Compiler.jsx starts sim, but we shouldn't need to?
+//         // SIM.StartSimulation();
+//         // if (DBG) console.log(...PR('SIMULATION STARTED'));
+//       })();
+//       resolve();
+//     })
+// );
+
+/// CLASS DECLARATION /////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/// NOTE: STYLES ARE IMPORTED FROM COMMON-STYLES.JS
+class PanelScript extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: 'Script',
+      script: demoscript
+    };
+    // codejar
+    this.jarRef = React.createRef();
+    this.jar = '';
+
+    // The keys map to token definitions in the prism css file.
+    Prism.languages.gemscript = Prism.languages.extend('javascript', {
+      'keyword': keywords_regex,
+      'namespace': /^# \W*/gm, // multiline
+      'inserted': types_regex
+    });
+
+    this.OnSaveClick = this.OnSaveClick.bind(this);
+  }
+
+  componentDidMount() {
+    // start URSYS
+    UR.SystemConfig({ autoRun: true });
+    // initialize codejar
+    const highlight = editor => {
+      Prism.highlightElement(editor);
+    };
+    const editor = this.jarRef.current;
+    this.jar = CodeJar(editor, highlight);
+    this.jar.onUpdate(code => {
+      this.text = code;
+    });
+  }
+
+  OnSaveClick() {
+    // This should save to URSYS
+    // HACK now to go back to select screen
+    const { onClick } = this.props;
+    onClick('select');
+  }
+
+  render() {
+    const { title, script } = this.state;
+    const { id, onClick, classes } = this.props;
+
+    const SaveBtn = (
+      <button
+        type="button"
+        className={classes.button}
+        style={{ alignSelf: 'flex-end' }}
+        onClick={this.OnSaveClick}
+      >
+        SAVE TO SERVER
+      </button>
+    );
+
+    return (
+      <PanelChrome
+        id={id} // used by click handler to identify panel
+        title={title}
+        onClick={onClick}
+        bottombar={SaveBtn}
+      >
+        <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+          <pre
+            className="language-gemscript line-numbers match-braces"
+            style={{
+              fontSize: '10px',
+              lineHeight: 1,
+              whiteSpace: 'pre-line'
+            }}
+          >
+            <code
+              id="codejar"
+              ref={this.jarRef}
+              style={{ width: '100%', height: 'auto' }}
+            >
+              {script}
+            </code>
+          </pre>
+        </div>
+      </PanelChrome>
+    );
+  }
+}
+
+export default withStyles(useStylesHOC)(PanelScript);
