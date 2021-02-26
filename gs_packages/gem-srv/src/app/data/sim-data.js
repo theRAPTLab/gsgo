f4bd18c2d87949b19cae2080caa08e6152b37714
Ben Loh <ben@inquirium.net>
2021-02-25 21:18:39 -0800

    mapeditor: SimData supports adding and updating instances via URSYS calls.

--- gs_packages/gem-srv/src/app/data/sim-data.js
+++ gs_packages/gem-srv/src/app/data/sim-data.js
@@ -7,6 +7,9 @@
 //
 // This just listens for requests via URSYS and sends data back.
 // This should be replaced by a proper database module.
+//
+// As of 2021-02-25, the instance updates just update
+// instances for the current session.  No data is saved.
 
 import UR from '@gemstep/ursys/client';
 
@@ -20,12 +23,30 @@ import { MODEL as BeesModel } from './bees';
 class SimData {
   constructor() {
     this.GetSimDataModel = this.GetSimDataModel.bind(this);
-    this.RequestSimDataModels = this.RequestSimDataModels.bind(this);
-    this.RequestSimDataModel = this.RequestSimDataModel.bind(this);
+    this.SendSimDataModels = this.SendSimDataModels.bind(this);
+    this.HandleSimDataModelRequest = this.HandleSimDataModelRequest.bind(this);
+    this.SendSimDataModel = this.SendSimDataModel.bind(this);
     this.HackGetSimDataModel = this.HackGetSimDataModel.bind(this);
+    this.InstanceAdd = this.InstanceAdd.bind(this);
+    this.InstanceUpdatePosition = this.InstanceUpdatePosition.bind(this);
     // Register Listeners
-    UR.HandleMessage('HACK_SIMDATA_REQUEST_MODELS', this.RequestSimDataModels);
-    UR.HandleMessage('HACK_SIMDATA_REQUEST_MODEL', this.RequestSimDataModel);
+    UR.HandleMessage('NET:INSTANCE_ADD', this.InstanceAdd);
+    UR.HandleMessage('NET:INSTANCE_UPDATE_POSITION', this.InstanceUpdatePosition);
+    UR.HandleMessage('HACK_SIMDATA_REQUEST_MODELS', this.SendSimDataModels);
+    UR.HandleMessage(
+      'HACK_SIMDATA_REQUEST_MODEL',
+      this.HandleSimDataModelRequest
+    );
+  }
+
+  componentWillUnmount() {
+    UR.UnhandleMessage('NET:INSTANCE_ADD', this.InstanceAdd);
+    UR.UnhandleMessage('NET:INSTANCE_UPDATE', this.InstanceUpdatePosition);
+    UR.UnhandleMessage('HACK_SIMDATA_REQUEST_MODELS', this.SendSimDataModels);
+    UR.UnhandleMessage(
+      'HACK_SIMDATA_REQUEST_MODEL',
+      this.HandleSimDataModelRequest
+    );
   }
 
   GetSimDataModel(modelId) {
@@ -56,7 +77,7 @@ class SimData {
    *
    * @param {Object} data -- { modelId: <string> }
    */
-  RequestSimDataModels() {
+  SendSimDataModels() {
     let models = [
       { id: 'aquatic', label: 'Aquatic Ecosystems' },
       { id: 'decomposition', label: 'Decomposition' },
@@ -71,13 +92,48 @@ class SimData {
    *
    * @param {Object} data -- { modelId: <string> }
    */
-  RequestSimDataModel(data) {
-    let model = this.GetSimDataModel(data.modelId);
+  HandleSimDataModelRequest(data) {
+    this.SendSimDataModel(data.modelId);
+  }
+  SendSimDataModel(modelId) {
+    let model = this.GetSimDataModel(modelId);
     UR.RaiseMessage('HACK_SIMDATA_UPDATE_MODEL', { model });
   }
 
   HackGetSimDataModel(modelId) {
     return this.GetSimDataModel(modelId);
+  }
+
+  /**
+   *
+   * @param {Object} data -- { modelId, blueprintName }
+   */
+  InstanceAdd(data) {
+    const model = this.GetSimDataModel(data.modelId);
+    const instance = {
+      name: `${data.blueprintName}${model.instances.length}`,
+      blueprint: data.blueprintName,
+      init: `prop agent.x setTo ${Math.trunc(Math.random() * 50 - 25)}
+prop agent.y setTo ${Math.trunc(Math.random() * 50 - 25)}`
+    };
+    model.instances.push(instance);
+    this.SendSimDataModel(data.modelId);
+  }
+
+  /**
+   *
+   * @param {Object} data -- { modelId, instanceName, updatedData }
+   */
+  InstanceUpdatePosition(data) {
+    const model = this.GetSimDataModel(data.modelId);
+    const instanceIndex = model.instances.findIndex(
+      i => i.name === data.instanceName
+    );
+    const instance = model.instances[instanceIndex];
+    instance.init = `prop agent.x setTo ${data.updatedData.x}
+prop agent.y setTo ${data.updatedData.y}`;
+    model.instances[instanceIndex] = instance;
+    this.SendSimDataModel(data.modelId);
   }
 }
 
