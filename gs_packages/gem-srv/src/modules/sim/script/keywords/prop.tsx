cf1ce6941e98e98d3a3f15fd3f708d9ad26f067f
Ben Loh <ben@inquirium.net>
2021-04-01 09:56:13 -0700

    props-add: Pull out reusable InputElement from props.tsx

--- gs_packages/gem-srv/src/modules/sim/script/keywords/prop.tsx
+++ gs_packages/gem-srv/src/modules/sim/script/keywords/prop.tsx
@@ -23,6 +23,7 @@ import { IAgent, IState, TOpcode, TScriptUnit } from 'lib/t-script';
 import { RegisterKeyword } from 'modules/datacore';
 import { withStyles } from '@material-ui/core/styles';
 import { useStylesHOC } from 'app/pages/elements/page-xui-styles';
+import InputElement from '../components/InputElement.tsx';
 
 /// CLASS HELPERS /////////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -34,17 +35,20 @@ const DBG = true;
  *  in the local state.
  *
  *  WIP: Currently this component can only handle setTo method.
+ *
+ *  NOTE: PropElement shares the same state as the `prop` keyword class
+ *
  */
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 type MyState = {
   propName: string;
   methodName: string;
   args: string[];
-  isDirty: boolean;
 };
 type MyProps = {
   index: number;
   state: MyState;
+  type: string;
   isEditable: boolean;
   isDeletable: boolean;
   serialize: (state: MyState) => TScriptUnit;
@@ -60,34 +64,8 @@ class PropElement extends React.Component<MyProps, MyState> {
     this.index = index;
     this.state = { ...state }; // copy state prop
     this.serialize = serialize;
-    this.onChange = this.onChange.bind(this);
-    this.onKeyDown = this.onKeyDown.bind(this);
-    this.onBlur = this.onBlur.bind(this);
-    this.saveData = this.saveData.bind(this);
-    this.onClick = this.onClick.bind(this);
     this.onDeleteLine = this.onDeleteLine.bind(this);
-  }
-  componentWillUnmount() {
-    const { isEditable } = this.props;
-    if (isEditable) this.saveData();
-  }
-  onChange(e) {
-    this.setState({
-      args: [e.currentTarget.value],
-      isDirty: true
-    });
-  }
-  onKeyDown(e) {
-    if (e.key === 'Enter') this.saveData(true);
-  }
-  onBlur() {
-    this.saveData();
-  }
-  onClick(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    // Stop click here when user clicks inside form to edit.
-    // Other clicks will propagage to InstanceEditor where it will exit edit mode
+    this.saveData = this.saveData.bind(this);
   }
   onDeleteLine(e) {
     e.preventDefault(); // prevent click from deselecting instance
@@ -101,8 +79,6 @@ class PropElement extends React.Component<MyProps, MyState> {
    *                           Used to handle exiting edit on "Enter"
    */
   saveData(exitEdit = false) {
-    const { isDirty } = this.state;
-    if (!isDirty) return;
     const updata = {
       index: this.index,
       scriptUnit: this.serialize(this.state),
@@ -111,50 +87,151 @@ class PropElement extends React.Component<MyProps, MyState> {
     UR.RaiseMessage('SCRIPT_UI_CHANGED', updata);
   }
   render() {
-    const { index, isEditable, isDeletable, classes } = this.props;
+    const { index, type, isEditable, isDeletable, classes } = this.props;
     const { propName, methodName, args } = this.state;
-    let jsx;
-    // HACK force number for now
-    // In the future check for argument type
-    const type = 'number';
-    if (isEditable) {
-      // Show Form
-      jsx = (
-        <div style={{ display: 'grid', gridTemplateColumns: '80px auto 15px' }}>
-          <div className={classes.instanceEditorLabel}>{propName}</div>
-          <input
-            onChange={this.onChange}
-            onKeyDown={this.onKeyDown}
-            onBlur={this.onBlur}
-            onClick={this.onClick}
-            type={type}
-            value={args[0]}
-            className={classes.instanceEditorField}
-          />
-          {isDeletable && (
-            <div className={classes.instanceEditorLine}>
-              <button
-                type="button"
-                className={classes.buttonMini}
-                onClick={this.onDeleteLine}
-              >
-                <DeleteIcon fontSize="small" />
-              </button>
-            </div>
-          )}
-        </div>
-      );
-    } else {
-      // Show Static Value
-      jsx = (
-        <>
-          {propName}:&nbsp;{args[0]}&nbsp;{' '}
-        </>
-      );
-    }
-    return jsx;
+    return (
+      <>
+        {!isEditable ? (
+          <>
+            {propName}:&nbsp;{args[0]}&nbsp;{' '}
+          </>
+        ) : (
+          <div style={{ display: 'grid', gridTemplateColumns: '80px auto 15px' }}>
+            <div className={classes.instanceEditorLabel}>{propName}</div>
+            {/* prop {propName} setTo */}
+            {args.map((arg, i) => (
+              <InputElement
+                state={this.state}
+                type={type}
+                onSave={this.saveData}
+                index={index}
+                argindex={i}
+                key={i}
+              />
+            ))}
+            {isDeletable && (
+              <div className={classes.instanceEditorLine}>
+                <button
+                  type="button"
+                  className={classes.buttonMini}
+                  onClick={this.onDeleteLine}
+                >
+                  <DeleteIcon fontSize="small" />
+                </button>
+              </div>
+            )}
+          </div>
+        )}
+      </>
+    );
   }
 } // end script element
+
+// ORIG CLASS
+//
+// class PropElement extends React.Component<MyProps, MyState> {
+//   index: number; // ui index
+//   keyword: string; // keyword
+//   serialize: (state: MyState) => TScriptUnit;
+//   constructor(props: MyProps) {
+//     super(props);
+//     const { index, state, serialize } = props;
+//     this.index = index;
+//     this.state = { ...state }; // copy state prop
+//     this.serialize = serialize;
+//     this.onChange = this.onChange.bind(this);
+//     this.onKeyDown = this.onKeyDown.bind(this);
+//     this.onBlur = this.onBlur.bind(this);
+//     this.saveData = this.saveData.bind(this);
+//     this.onClick = this.onClick.bind(this);
+//     this.onDeleteLine = this.onDeleteLine.bind(this);
+//   }
+//   componentWillUnmount() {
+//     const { isEditable } = this.props;
+//     if (isEditable) this.saveData();
+//   }
+//   onChange(e) {
+//     this.setState({
+//       args: [e.currentTarget.value],
+//       isDirty: true
+//     });
+//   }
+//   onKeyDown(e) {
+//     if (e.key === 'Enter') this.saveData(true);
+//   }
+//   onBlur() {
+//     this.saveData();
+//   }
+//   onClick(e) {
+//     e.preventDefault();
+//     e.stopPropagation();
+//     // Stop click here when user clicks inside form to edit.
+//     // Other clicks will propagage to InstanceEditor where it will exit edit mode
+//   }
+//   onDeleteLine(e) {
+//     e.preventDefault(); // prevent click from deselecting instance
+//     e.stopPropagation();
+//     const updata = { index: this.index };
+//     UR.RaiseMessage('SCRIPT_LINE_DELETE', updata);
+//   }
+//   /**
+//    *
+//    * @param {boolean} exitEdit Tell InstanceEditor to exit edit mode.
+//    *                           Used to handle exiting edit on "Enter"
+//    */
+//   saveData(exitEdit = false) {
+//     const { isDirty } = this.state;
+//     if (!isDirty) return;
+//     const updata = {
+//       index: this.index,
+//       scriptUnit: this.serialize(this.state),
+//       exitEdit
+//     };
+//     UR.RaiseMessage('SCRIPT_UI_CHANGED', updata);
+//   }
+//   render() {
+//     const { index, type, isEditable, isDeletable, classes } = this.props;
+//     const { propName, methodName, args } = this.state;
+//     let jsx;
+//     if (isEditable) {
+//       // Show Form
+//       jsx = (
+//         <div style={{ display: 'grid', gridTemplateColumns: '80px auto 15px' }}>
+//           <div className={classes.instanceEditorLabel}>{propName}</div>
+//           <input
+//             onChange={this.onChange}
+//             onKeyDown={this.onKeyDown}
+//             onBlur={this.onBlur}
+//             onClick={this.onClick}
+//             type={type}
+//             value={args[0]}
+//             className={classes.instanceEditorField}
+//           />
+//           {isDeletable && (
+//             <div className={classes.instanceEditorLine}>
+//               <button
+//                 type="button"
+//                 className={classes.buttonMini}
+//                 onClick={this.onDeleteLine}
+//               >
+//                 <DeleteIcon fontSize="small" />
+//               </button>
+//             </div>
+//           )}
+//         </div>
+//       );
+//     } else {
+//       // Show Static Value
+//       jsx = (
+//         <>
+//           {propName}:&nbsp;{args[0]}&nbsp;{' '}
+//         </>
+//       );
+//     }
+//     return jsx;
+//   }
+// } // end script element
+
 /// GEMSCRIPT KEYWORD DEFINITION //////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 export class prop extends Keyword {
@@ -198,17 +275,21 @@ export class prop extends Keyword {
     const state = {
       propName: ref,
       methodName,
-      args,
-      isDirty: false
+      args
     };
+    // HACK force number for now
+    // In the future check for argument type
+    //    const type = 'number';
+    const type = 'number';
     const isEditable = children ? children.isEditable : false;
     const isDeletable = children ? children.isDeletable : false;
     const StyledPropElement = withStyles(useStylesHOC)(PropElement);
     return (
       <StyledPropElement
+        state={state}
         index={index}
         key={index}
-        state={state}
+        type={type}
         isEditable={isEditable}
         isDeletable={isDeletable}
         serialize={this.serialize}
