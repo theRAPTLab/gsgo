1c95a9681253061dcf7e7a2dfeb089e3cafd84ca
Ben Loh <ben@inquirium.net>
2021-04-17 22:04:38 -0700

    boundary: Change all random number calls to m_random for future seeding.

--- gs_packages/gem-srv/src/modules/sim/features/feat-movement.ts
+++ gs_packages/gem-srv/src/modules/sim/features/feat-movement.ts
@@ -5,6 +5,12 @@
   This is the "FeaturePack" base class, which you can extend to implement
   your own features.
 
+  Always use m_Random to generate random values.
+
+  Direction
+    0  = right
+    90 = up
+
   TODO: add methods for initialization management
 
 \*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ * /////////////////////////////////////*/
@@ -33,8 +39,8 @@ function moveJitter(
   max: number = 5,
   round: boolean = true
 ) {
-  const x = m_Random(min, max, round);
-  const y = m_Random(min, max, round);
+  const x = m_random(min, max, round);
+  const y = m_random(min, max, round);
   m_setPosition(agent, agent.prop.x.value + x, agent.prop.y.value + y);
 }
 
@@ -44,8 +50,8 @@ function moveWander(agent) {
   // but really change direction once in a while
   const distance = agent.prop.Movement.distance.value;
   let direction = agent.prop.Movement.direction.value;
-  if (Math.random() > 0.98) {
-    direction += Math.random() * 180;
+  if (m_random() > 0.98) {
+    direction += m_random(-90, 90);
     agent.prop.Movement.direction.value = direction;
   }
   const angle = m_DegreesToRadians(direction);
@@ -164,7 +170,7 @@ class MovementPack extends GFeature {
           agent.getFeatProp(this.name, 'distance').value = params[0];
           agent.getFeatProp(this.name, 'direction').value = params[1];
           if (params[2] == 'rand')
-            agent.getFeatProp(this.name, 'direction').value = Math.random() * 180;
+            agent.getFeatProp(this.name, 'direction').value = m_random(0, 180);
           break;
         case 'jitter':
           // min max
@@ -176,17 +182,17 @@ class MovementPack extends GFeature {
 
   setDirection(agent: IAgent, direction: number) {
     console.log('setting direction to', direction);
-    agent.getFeatProp(this.name, 'direction').value = direction;
+    m_setDirection(agent, direction);
   }
 
   setRandomDirection(agent: IAgent) {
-    agent.getFeatProp(this.name, 'direction').value = Math.random() * 360;
+    m_setDirection(agent, m_random(0, 360));
   }
 
   setRandomPosition(agent: IAgent) {
     const bounds = PROJ.GetBounds();
-    const x = m_Random(bounds.left, bounds.right);
-    const y = m_Random(bounds.top, bounds.bottom);
+    const x = m_random(bounds.left, bounds.right);
+    const y = m_random(bounds.top, bounds.bottom);
     m_setPosition(agent, x, y);
   }
 
@@ -196,15 +202,16 @@ class MovementPack extends GFeature {
   }
 
   jitterPos(agent, min: number = -5, max: number = 5, round: boolean = true) {
-    const x = m_Random(min, max, round);
-    const y = m_Random(min, max, round);
+    const x = m_random(min, max, round);
+    const y = m_random(min, max, round);
     m_setPosition(agent, agent.prop.x.value + x, agent.prop.y.value + y);
   }
 } // end of feature class
 
 /// CLASS HELPERS /////////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-function m_Random(min, max, round = true) {
+function m_random(min = 0, max = 1, round = false) {
+  // REVIEW: Replace with a seeded random number generator
   const n = Math.random() * (max - min) + min;
   if (round) return Math.round(n);
   return n;
@@ -247,6 +254,10 @@ function m_setPosition(agent, x, y) {
   }
   agent.prop.x.value = xx;
   agent.prop.y.value = yy;
+}
+function m_setDirection(agent, degrees) {
+  console.error('direction', degrees);
+  agent.prop.Movement.direction.value = degrees;
 }
 
 /// REGISTER SINGLETON ////////////////////////////////////////////////////////
