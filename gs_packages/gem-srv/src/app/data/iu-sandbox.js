2805d3e516dd28b603915ece4e0a84e59c96bc80
Joshua Danish <jdanish@indiana.edu>
2021-05-15 17:49:20 -0400

    Adding some people to sandbox

--- gs_packages/gem-srv/src/app/data/iu-sandbox.js
+++ gs_packages/gem-srv/src/app/data/iu-sandbox.js
@@ -11,19 +11,50 @@ export const MODEL = {
   },
   scripts: [
     {
-      id: 'Person',
-      label: 'Person',
+      id: 'GPerson',
+      label: 'Green Person',
       isControllable: true,
-      script: `# BLUEPRINT Person
+      script: `# BLUEPRINT GPerson
 # PROGRAM DEFINE
 useFeature Costume
-featCall Costume setCostume 'bunny.json' 1
+featCall Costume setCostume 'bunny.json' 2
 
 useFeature Physics
-useFeature Touches
 featCall Physics init
 
-featCall Physics setSize 90
+featProp Physics scale setTo 2
+
+# PROGRAM UPDATE
+`
+    },
+    {
+      id: 'BPerson',
+      label: 'Blue Person',
+      isControllable: true,
+      script: `# BLUEPRINT BPerson
+# PROGRAM DEFINE
+useFeature Costume
+featCall Costume setCostume 'bunny.json' 3
+
+useFeature Physics
+featCall Physics init
+
+featProp Physics scale setTo 2
+
+# PROGRAM UPDATE
+`
+    },
+    {
+      id: 'OfficeStuff',
+      label: 'OfficeStuff',
+      isControllable: false,
+      script: `# BLUEPRINT OfficeStuff
+# PROGRAM DEFINE
+useFeature Costume
+featCall Costume setCostume 'hive.json' 0
+
+useFeature Physics
+featCall Physics init
 
 # PROGRAM UPDATE
 `
@@ -32,10 +63,26 @@ featCall Physics setSize 90
   instances: [
     {
       id: 1101,
-      name: 'Person 1',
-      blueprint: 'Person',
+      name: 'Green Person 1',
+      blueprint: 'GPerson',
       initScript: `prop x setTo -200
     prop y setTo 0
+    `
+    },
+    {
+      id: 1102,
+      name: 'Blue Person 1',
+      blueprint: 'BPerson',
+      initScript: `prop x setTo 200
+    prop y setTo 0
+    `
+    },
+    {
+      id: 1103,
+      name: 'Furniture',
+      blueprint: 'OfficeStuff',
+      initScript: `prop x setTo 300
+    prop y setTo 300
     `
     }
   ]
