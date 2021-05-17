33e1a858e1344177ceb83f64858977c76908d1f7
Joshua Danish <jdanish@indiana.edu>
2021-05-17 10:26:16 -0400

    Added a new asset and some script to iu-sandbox. The assets will need to be transferred outside the repo for now.
    ** Do not try and run for a pilot test given the merge of AgentCreate not yet working.

--- gs_packages/gem-srv/src/app/data/iu-sandbox.js
+++ gs_packages/gem-srv/src/app/data/iu-sandbox.js
@@ -81,9 +81,46 @@ featCall Physics init
       id: 1103,
       name: 'Furniture',
       blueprint: 'OfficeStuff',
-      initScript: `prop x setTo 270
-    prop y setTo 270
+      initScript: `prop x setTo 348
+    prop y setTo 83
     prop zIndex setTo -100
+    featProp Physics scale setTo 0.4
+    featProp Physics scaleY setTo 2.5
+    `
+    },
+    {
+      id: 1104,
+      name: 'Furniture',
+      blueprint: 'OfficeStuff',
+      initScript: `prop x setTo -348
+    prop y setTo 83
+    prop zIndex setTo -100
+    featProp Physics scale setTo 0.4
+    featProp Physics scaleY setTo 2.5
+    `
+    },
+    {
+      id: 1105,
+      name: 'Blue Person 2',
+      blueprint: 'BPerson',
+      initScript: `prop x setTo 210
+    prop y setTo 119
+    `
+    },
+    {
+      id: 1106,
+      name: 'Blue Person 3',
+      blueprint: 'BPerson',
+      initScript: `prop x setTo 230
+    prop y setTo 234
+    `
+    },
+    {
+      id: 1107,
+      name: 'Blue Person 4',
+      blueprint: 'BPerson',
+      initScript: `prop x setTo 220
+    prop y setTo 322
     `
     }
   ]
