017c2bfced5b8d7eb3e75443e3b2e89ac3105579
Ben Loh <ben@inquirium.net>
2021-05-21 14:33:28 -0700

    forage: Add foliage agents.

--- gs_packages/gem-srv/src/app/data/moths.js
+++ gs_packages/gem-srv/src/app/data/moths.js
@@ -14,8 +14,11 @@ prop alpha setTo 1
 
 useFeature Movement
 useFeature Physics
+featProp Physics scale setTo 0.5
+
 useFeature Touches
-featCall Touches monitorTouchesWith Tree
+featCall Touches monitorTouchesWith TreeTrunk
+featCall Touches monitorTouchesWith TreeFoliage
 
 // allow removal by  Predator
 useFeature Population
@@ -25,9 +28,21 @@ featCall Costume randomizeColor 0.1 0.3 0.1
 featCall Movement setRandomStart
 
 # PROGRAM UPDATE
-prop alpha setTo 1
-when Moth touches Tree [[
-  prop alpha setTo 0.2
+when Moth touches TreeTrunk [[
+  ifExpr {{ !Moth.getFeatProp('Movement', 'isMoving').value }} [[
+    prop alpha setTo 0.1
+  ]]
+  ifExpr {{ Moth.getFeatProp('Movement', 'isMoving').value }} [[
+    prop alpha setTo 1
+  ]]
+]]
+when Moth touches TreeFoliage [[
+  ifExpr {{ !Moth.getFeatProp('Movement', 'isMoving').value }} [[
+    prop alpha setTo 0.1
+  ]]
+  ifExpr {{ Moth.getFeatProp('Movement', 'isMoving').value }} [[
+    prop alpha setTo 1
+  ]]
 ]]
 `
     },
@@ -38,7 +53,7 @@ when Moth touches Tree [[
       script: `# BLUEPRINT Predator
 # PROGRAM DEFINE
 useFeature Costume
-featCall Costume setCostume 'bunny.json' 0
+featCall Costume setCostume 'bee.json' 0
 
 useFeature Physics
 useFeature Touches
@@ -58,12 +73,27 @@ when Predator touches Moth [[
 `
     },
     {
-      id: 'Tree',
-      label: 'Tree',
-      script: `# BLUEPRINT Tree
+      id: 'TreeTrunk',
+      label: 'TreeTrunk',
+      script: `# BLUEPRINT TreeTrunk
 # PROGRAM DEFINE
 useFeature Costume
 featCall Costume setCostume 'lightbeam.json' 0
+featCall Costume setColorize 0.2 0.3 0
+
+useFeature Physics
+
+# PROGRAM INIT
+prop zIndex setTo -200
+`
+    },
+    {
+      id: 'TreeFoliage',
+      label: 'TreeFoliage',
+      script: `# BLUEPRINT TreeFoliage
+# PROGRAM DEFINE
+useFeature Costume
+featCall Costume setCostume 'circle.json' 0
 featCall Costume setColorize 0 1 0
 
 useFeature Physics
@@ -77,32 +107,62 @@ prop zIndex setTo -200
     {
       id: 1101,
       name: 'Tree1',
-      blueprint: 'Tree',
+      blueprint: 'TreeTrunk',
       initScript: `prop x setTo -200
-prop y setTo 0
-featCall Costume setColorize 0 0.8 0
-featProp Physics scale setTo 0.5
-featProp Physics scaleY setTo 4`
+prop y setTo 200
+featCall Costume setColorize 0.2 0.1 0
+featProp Physics scale setTo 0.3
+featProp Physics scaleY setTo 2`
     },
     {
       id: 1102,
-      name: 'Tree2',
-      blueprint: 'Tree',
-      initScript: `prop x setTo 0
-prop y setTo 0
-featCall Costume setColorize 0.2 0.9 0
-featProp Physics scale setTo 0.8
-featProp Physics scaleY setTo 4`
+      name: 'TreeFoliage1',
+      blueprint: 'TreeFoliage',
+      initScript: `prop x setTo -200
+prop y setTo -150
+featCall Costume setColorize 0.2 0.8 0
+featProp Physics scale setTo 1.8
+featProp Physics scaleY setTo 1.7`
+    },
+    {
+      id: 1105,
+      name: 'Tree3',
+      blueprint: 'TreeTrunk',
+      initScript: `prop x setTo 250
+prop y setTo 200
+featCall Costume setColorize 0.4 0.2 0
+featProp Physics scale setTo 0.4
+featProp Physics scaleY setTo 2`
+    },
+    {
+      id: 1106,
+      name: 'TreeFoliage3',
+      blueprint: 'TreeFoliage',
+      initScript: `prop x setTo 250
+prop y setTo -150
+featCall Costume setColorize 0.3 0.7 0
+featProp Physics scale setTo 1.4
+featProp Physics scaleY setTo 1.8`
     },
     {
       id: 1103,
-      name: 'Tree3',
-      blueprint: 'Tree',
-      initScript: `prop x setTo 250
-prop y setTo 0
-featCall Costume setColorize 0 0.9 0.3
-featProp Physics scale setTo 0.4
-featProp Physics scaleY setTo 4`
+      name: 'Tree2',
+      blueprint: 'TreeTrunk',
+      initScript: `prop x setTo 0
+prop y setTo 200
+featCall Costume setColorize 0.2 0.2 0
+featProp Physics scale setTo 0.6
+featProp Physics scaleY setTo 2`
+    },
+    {
+      id: 1104,
+      name: 'TreeFoliage2',
+      blueprint: 'TreeFoliage',
+      initScript: `prop x setTo 0
+prop y setTo -150
+featCall Costume setColorize 0.2 0.7 0
+featProp Physics scale setTo 2
+featProp Physics scaleY setTo 2`
     },
     {
       id: 1201,
