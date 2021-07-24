c5d1b1698227dc28f1361ba3edd6bf836052c72c
Corey Brady <corey.brady@vanderbilt.edu>
2021-07-24 17:40:35 -0500

    add pozyx config settings for VU in commented out state

--- gs_packages/gem-srv/src/modules/datacore/dc-inputs.ts
+++ gs_packages/gem-srv/src/modules/datacore/dc-inputs.ts
@@ -141,6 +141,17 @@ function UpdateActiveDevices(changes) {
 }
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+/// POZYX DATA UPDATE  FOR VU/////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/* export const POZYX_TRANSFORM = {
+  scaleX: -0.0002, // -0.0002
+  scaleY: 0.00016, // 0.0003
+  translateX: -4200, //0
+  translateY: -6000, //0
+  rotate: 0, // -160
+  useAccelerometer: true
+}; */
+
 /// POZYX DATA UPDATE /////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 export const POZYX_TRANSFORM = {
