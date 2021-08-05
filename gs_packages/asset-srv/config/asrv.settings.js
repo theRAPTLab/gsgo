31081f6e44bf77f40604a7a54f5dbd91fed41a5d
DSri Seah <dsri@dsriseah.com>
2021-08-05 16:43:38 -0400

    asset-server: change public_resources to gs_assets

--- gs_packages/asset-srv/config/asrv.settings.js
+++ gs_packages/asset-srv/config/asrv.settings.js
@@ -1,6 +1,6 @@
 const PACKAGE_NAME = 'ASSET_SRV';
 // public path is relative to the gsgo top level
-const PUBLIC_RESOURCES_PATH = 'public_resources';
+const PUBLIC_RESOURCES_PATH = 'gs_assets';
 
 module.exports = {
   PACKAGE_NAME,
