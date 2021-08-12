69e0c679d5bffb2c30d6794d15eff8c90e23f526
Joshua Danish <jdanish@indiana.edu>
2021-08-12 13:50:20 -0400

    IN settings on inputs and tracking stuff

--- gs_packages/gem-srv/server/step-tracker.js
+++ gs_packages/gem-srv/server/step-tracker.js
@@ -329,7 +329,7 @@ function m_BindPozyxListener() {
   // });
 
   // UNCOMMENT THIS TO ALLOW LOCALHOST for the mqtt broker to run locallly (replay of pozyx streams)
-  mtrack_ss = mqtt.connect('mqtt://localhost', {
+  mtrack_ss = mqtt.connect('mqtt://10.0.0.254/', {
     port: 1883
   }); // Enterprise server "via uplink network" works
 
--- gs_packages/gem-srv/src/modules/datacore/dc-inputs.ts
+++ gs_packages/gem-srv/src/modules/datacore/dc-inputs.ts
@@ -145,17 +145,24 @@ function UpdateActiveDevices(changes) {
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 // VU
-export const POZYX_TRANSFORM = {
+/* export const POZYX_TRANSFORM = {
   scaleX: -0.0002, // -0.0002
   scaleY: 0.00016, // 0.0003
   translateX: -4200, //0
   translateY: -6000, //0
   rotate: 0, // -160
   useAccelerometer: true
-};
+}; */
 
 // IU
-// add when finished
+export const POZYX_TRANSFORM = {
+  scaleX: -0.0004, // -0.0002
+  scaleY: 0.0002, // 0.0003
+  translateX: -1522, //0
+  translateY: -2639, //0
+  rotate: 0, // -160
+  useAccelerometer: true
+};
 
 // BEN
 /* export const POZYX_TRANSFORM = {
