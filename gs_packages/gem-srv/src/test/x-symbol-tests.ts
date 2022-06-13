d47c9546bb80994b7f99f6a3bc9f0c8429f1be1c
DSri Seah <dsri@dsriseah.com>
2022-06-13 18:31:22 -0400

    dev-next-gui: restore DevWizard default to 'gui-wizard-slots.gemscript' test script

--- gs_packages/gem-srv/src/test/x-symbol-tests.ts
+++ gs_packages/gem-srv/src/test/x-symbol-tests.ts
@@ -12,8 +12,8 @@ import * as PROJ_v2 from 'modules/datacore/dc-project-v2';
 import * as CHECK from 'modules/datacore/dc-sim-data-utils';
 import { ENABLE_SYMBOL_TEST_BLUEPRINT } from 'config/dev-settings';
 import { DEV_PRJID, DEV_BPID } from 'config/gem-settings';
-// import TEST_SCRIPT from 'test/gemscript/gui-wizard-slots.gemscript';
-import TEST_SCRIPT from 'test/gemscript/gui-wizard-deref.gemscript';
+import TEST_SCRIPT from 'test/gemscript/gui-wizard-slots.gemscript';
+// import TEST_SCRIPT from 'test/gemscript/gui-wizard-deref.gemscript';
 
 const { warn, log, table, group, groupCollapsed, groupEnd } = console;
 
