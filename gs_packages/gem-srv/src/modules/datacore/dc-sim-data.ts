fe89aa8fc2fe334c4f5859286a4b3f965da9962d
DSri Seah <dsri@dsriseah.com>
2022-06-25 20:09:59 -0400

    dev-sri/integration-0625: add missing GetAgentBoundingRect() to feat-vision

--- gs_packages/gem-srv/src/modules/datacore/dc-sim-data.ts
+++ gs_packages/gem-srv/src/modules/datacore/dc-sim-data.ts
@@ -332,25 +332,6 @@ function GetAllFunctions() {
   const list = [...NAMED_FUNCTIONS.entries()];
   return list;
 }
-
-/// DEPRECATED - MOVE TO FEAT VISION //////////////////////////////////////////
-/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-/// Returns the SCALED bounding rect of the agent
-export function GetAgentBoundingRect(agent) {
-  // Based on costume
-  if (!agent.hasFeature('Costume'))
-    throw new Error(
-      `GetAgentBoundingRect: Tried to use vision on an agent with no costume ${agent.id}`
-    );
-  const { w, h } = agent.callFeatMethod('Costume', 'getScaledBounds');
-  const halfw = w / 2;
-  const halfh = h / 2;
-  return [
-    { x: agent.x - halfw, y: agent.y - halfh },
-    { x: agent.x + halfw, y: agent.y - halfh },
-    { x: agent.x + halfw, y: agent.y + halfh },
-    { x: agent.x - halfw, y: agent.y + halfh }
-  ];
 }
 
 /// SCRIPT EVENTS /////////////////////////////////////////////////////////////
