82989a42bf83008c6e1646aeaa447878b934ba7f
DSri Seah <dsri@dsriseah.com>
2022-06-30 18:41:11 -0400

    sri/412-error-mgr: update api-error manager prior to directory move

--- gs_packages/gem-srv/src/modules/error/api-error.ts
+++ gs_packages/gem-srv/src/modules/error/api-error.ts
@@ -9,34 +9,40 @@
 
 /// TYPE DEFINITIONS //////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-type SMErrType =
-  | `${'parser'}`
-  | `${'tokenizer'}`
-  | `${'eval'}`
+type SMErrSource =
+  | `${'project-loader'}`
+  | `${'expr-parser'}`
+  | `${'expr-eval'}`
+  | `${'script-parser'}`
   | `${'bundler'}`
-  | `${'compiler'}`;
-type SMErrMeta = {
+  | `${'compiler'}`
+  | `${'validator'}`
+  | `${'symbolizer'}`
+  | `${'simulator'}`;
+type SMErrCode = `${'bundler'}` | `${'bundler'}`;
+type SMErrContext = {
   info: string; // required information
   //
-  code?: string; // optional error code
-  line?: number; // the line where it happened (filled by bundler)
-  index?: number; // index into the line (filled by bundler)
+  code?: SMErrCode; // technical optional error code
+  module?: string; // user-friendly module name
+  line_num?: number; // the line where it happened (filled by bundler)
+  line_pos?: number; // index into the line (filled by bundler)
   [any: string]: any; // optional data to put into data object
 };
 
 /// HELPER CLASS DECLARATION //////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 class SMError {
-  type: SMErrType;
-  meta: SMErrMeta;
+  source: SMErrSource;
+  context: SMErrContext;
   data: any;
   timeStamp: number;
   //
-  constructor(type: SMErrType, meta?: SMErrMeta) {
-    this.type = type;
-    if (typeof meta === 'object') {
-      const { name, info, code, line, index, ...data } = meta;
-      this.meta = { name, info, code, line, index };
+  constructor(source: SMErrSource, err_ctx?: SMErrContext) {
+    this.source = source;
+    if (typeof err_ctx === 'object') {
+      const { name, info, code, line, index, ...data } = err_ctx;
+      this.context = { name, info, code, line, index };
       this.data = data;
     }
     this.timeStamp = Date.now();
@@ -49,10 +55,15 @@ const ERR_QUEUE: SMError[] = [];
 
 /// MODULE API ////////////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-function ERROR(type: SMErrType, info: SMErrMeta) {
-  ERR_QUEUE.push(new SMError(type, info));
+function ERROR(source: SMErrSource, info: SMErrContext) {
+  ERR_QUEUE.push(new SMError(source, info));
+}
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+function GetError() {
+  return ERR_QUEUE;
 }
 
 /// EXPORTS ///////////////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-export { ERROR };
+export default ERROR;
+export { GetError };
